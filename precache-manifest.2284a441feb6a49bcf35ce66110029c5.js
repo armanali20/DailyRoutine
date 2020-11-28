fdcb0a1a81df29bc20780b2b1e60155e2cc6bcc6
armanali20 <armanali20797@gmail.com>
2020-11-28 21:23:07 +0530

    Updates

---
+++ precache-manifest.2284a441feb6a49bcf35ce66110029c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a5415ea9c40983590396c5c61b8341d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "914dcbf42d32d556418d",
+    "url": "/static/css/main.a6d4a297.chunk.css"
+  },
+  {
+    "revision": "ddcc107ea624276ba84b",
+    "url": "/static/js/2.174a9f6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.174a9f6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "914dcbf42d32d556418d",
+    "url": "/static/js/main.d8499907.chunk.js"
+  },
+  {
+    "revision": "402f05c59b4e2a57dec5",
+    "url": "/static/js/runtime-main.d1310a82.js"
+  }
+]);
